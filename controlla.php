88a66c3b0787711cda5871dacb91fb4ab9003adf
Andrea Pizzi <andreapizzi90@outlook.com>
2021-05-20 11:19:13 +0200

    update controlla.php

--- controlla.php
+++ controlla.php
@@ -11,14 +11,14 @@
 			<img src= http://localhost/cryptoFantasy/good.gif   alt= 'Stregatto Felice' higth = 150 width= 150>
 			<p><i> E' questo il numero.....SI SI!</i></p>
 			<p>adesso calcolo la chiave di Alice.... eccola! {$_SESSION['alice']}</p>
-			<p>e adesso calcolo quella del bianconiglio.... {$_SESSION['bob']}</p>
+			
 			";
 			
 	}
 	else
 	{	echo "
 		<img src= http://localhost/cryptoFantasy/bad.gif alt = 'stregatto Triste' higth = 150 width= 150>
-                <p><i> Non credo sia questo.....</i></p>
+                <p><i> Non credo sia questo.....Ho no! Alice e Bianconiglio cambiano i numeri!</i></p>
 		";
 	}
 ?>
